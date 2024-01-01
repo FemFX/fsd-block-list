a53a10a3baf6288f8112a42f5f38ff72df909add
Иван Гнедько <ignedko34@gmail.com>
2024-01-01 12:44:05 +0300

    refactoring

--- server/src/account/account.dto.ts
+++ server/src/account/account.dto.ts
@@ -13,7 +13,7 @@ export class AccountDto {
   isBlockingEnabled: boolean;
 }
 export class PatchAccountDto {
-  @ApiProperty()
+  @ApiProperty({ required: false })
   @IsBoolean()
   @IsOptional()
   isBlockingEnabled?: boolean;
